d7f4076c00d9706e819edc580171449462918881
sechs <philippe.buteau@skema.edu>
2021-09-03 12:25:20 +0200

    line animation done

--- src/Components/WelcomeAnim/WelcomeAnim.js
+++ src/Components/WelcomeAnim/WelcomeAnim.js
@@ -1,12 +1,19 @@
 import React from "react";
 import "./WelcomeAnim.css";
+import { useRef } from "react";
 
 export default function WelcomeAnim() {
+  const bgEl = useRef();
+
+  const launchSite = () => {
+    bgEl.current.classList.add("active");
+  };
+
   return (
     <div className="container">
-      <div className="line"></div>
-      <div className="eye"></div>
-      <div className="circle"></div>
+      <div ref={bgEl} className="line"></div>
+      <div onClick={launchSite} className="eye"></div>
+      <div onClick={launchSite} className="circle"></div>
       <p className="enter">enter</p>
       <p className="enter-mirror">enter</p>
     </div>
