77ac08f6418c799e5dffcf30569b5ddb178957c1
sechs <philippe.buteau@skema.edu>
2021-09-03 12:51:20 +0200

    All styling of the welcome animation done

--- src/Components/WelcomeAnim/WelcomeAnim.js
+++ src/Components/WelcomeAnim/WelcomeAnim.js
@@ -3,19 +3,36 @@ import "./WelcomeAnim.css";
 import { useRef } from "react";
 
 export default function WelcomeAnim() {
-  const bgEl = useRef();
+  const bgEl = useRef(null);
+  const eyeEl = useRef(null);
+  const circleEl = useRef(null);
+  const enterEl = useRef(null);
+  const enterMirrorEl = useRef(null);
 
   const launchSite = () => {
     bgEl.current.classList.add("active");
+    enterEl.current.classList.add("active");
+    enterMirrorEl.current.classList.add("active");
+    eyeEl.current.style.cursor = "default";
+    circleEl.current.style.cursor = "default";
+
+    setTimeout(() => {
+      eyeEl.current.classList.add("active");
+      circleEl.current.classList.add("active");
+    }, 2000);
   };
 
   return (
     <div className="container">
       <div ref={bgEl} className="line"></div>
-      <div onClick={launchSite} className="eye"></div>
-      <div onClick={launchSite} className="circle"></div>
-      <p className="enter">enter</p>
-      <p className="enter-mirror">enter</p>
+      <div ref={eyeEl} onClick={launchSite} className="eye"></div>
+      <div ref={circleEl} onClick={launchSite} className="circle"></div>
+      <p ref={enterEl} className="enter">
+        enter
+      </p>
+      <p ref={enterMirrorEl} className="enter-mirror">
+        enter
+      </p>
     </div>
   );
 }
