b2354cc01316d332464a63ea49a3b2d20d5f2461
sechs <philippe.buteau@skema.edu>
2021-09-08 12:48:30 +0200

    prefixing client routes

--- src/Api/apiHandler.js
+++ src/Api/apiHandler.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const service = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL,
+  baseURL: process.env.REACT_APP_BACKEND_URL + "/api",
 });
 
 // eslint-disable-next-line import/no-anonymous-default-export
